8efcaeb97d431a34ee6db396c90e81f789b7457d
technicalhari <pandayhariom2898@gmail.com>
2020-09-27 03:56:05 +0530

    Updates

---
+++ precache-manifest.5a148666d9eecc5181a1fccf960eb3b4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da05956d920a58f26e4842768aa44772",
+    "url": "/iot-blogg/index.html"
+  },
+  {
+    "revision": "b0137c4663a46ab52e44",
+    "url": "/iot-blogg/static/css/main.c20f6844.chunk.css"
+  },
+  {
+    "revision": "378d42223fe9f4d980d0",
+    "url": "/iot-blogg/static/js/2.c6195682.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/iot-blogg/static/js/2.c6195682.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b0137c4663a46ab52e44",
+    "url": "/iot-blogg/static/js/main.b9baa61c.chunk.js"
+  },
+  {
+    "revision": "e6dba0a49194722d7f91",
+    "url": "/iot-blogg/static/js/runtime-main.f5ba6255.js"
+  }
+]);
