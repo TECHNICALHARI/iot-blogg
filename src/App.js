e1ff6026c11a56931409c5c79f9373b9b7fa8576
technicalhari <pandayhariom2898@gmail.com>
2020-09-27 03:48:07 +0530

    deploy my react app to git hub pages

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div>
+      <h1>Hello Hari Om Panday Welcome to my Blogg</h1>
+      <p>How are you man?</p>
+    </div>
+  )
+}
+
+export default App
